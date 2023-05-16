557aee0d06125c98e98fcf86a3ab91b50a20a995
Amira Azkia <99363263+Mir707@users.noreply.github.com>
2023-05-17 00:17:10 +0800

    Add files via upload

---
+++ lib/services/firebase_services.dart
@@ -0,0 +1,31 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:sprint1/pages/profile_page.dart';
+
+class FirebaseServices {
+  final FirebaseAuth auth = FirebaseAuth.instance;
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  CollectionReference users = FirebaseFirestore.instance.collection('users');
+  CollectionReference categories =
+      FirebaseFirestore.instance.collection('categories');
+
+  Future<void> updateUser(Map<String, dynamic> data, context) {
+    return users
+        .doc('currentUser.uid')
+        .update(data)
+        //.then(
+        //(value) {
+        // Navigator.pushNamed(context, );
+        //},
+        //)
+        .catchError((error) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Failed to update location'),
+        ),
+      );
+    });
+  }
+}
