d2da76c6970edd2dafb40b3c3594bbd17c49c654
CHIA JEE LIANG <93245148+jeeliang@users.noreply.github.com>
2023-05-17 08:23:45 +0800

    Add files via upload

--- lib/components/users.dart
+++ lib/components/users.dart
@@ -1,16 +1,36 @@
 import 'dart:io';
 
 class Users {
+  String id;
   final String fullName;
   final String shopName;
-  final String email;
   final String phone;
-  final File? imageFile;
+  final String imageUrl;
 
   Users(
-      {required this.fullName,
+    {
+      this.id ='',
+      required this.fullName,
       required this.shopName,
-      required this.email,
       required this.phone,
-      required this.imageFile});
+      required this.imageUrl
+      });
+
+    Map<String, dynamic> toJson() => {
+      'id': id,
+      'fullName' : fullName,
+      'shopName' : shopName,
+      'phone' : phone,
+      'imageUrl' : imageUrl
+    };
+
+    static Users fromJson(Map<String, dynamic> json) => Users(
+      id: json['id'],
+      fullName: json['fullName'],
+      shopName: json['shopName'],
+      phone: json['phone'],
+      imageUrl: json['imageUrl']
+    );
+
+  
 }
