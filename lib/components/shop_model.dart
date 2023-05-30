0f05ce0608612d33c0ed272b91bc7bbcc7ad0cee
Amira Azkia <99363263+Mir707@users.noreply.github.com>
2023-05-30 19:56:26 +0800

    Add files via upload

--- lib/components/shop_model.dart
+++ lib/components/shop_model.dart
@@ -1,97 +1,56 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
 
 class ShopModel extends StatelessWidget {
   final String shopName;
-  final String shopImage;
+  final String imageUrl;
 
-  const ShopModel({super.key, required this.shopName, required this.shopImage});
+  const ShopModel({Key? key, required this.shopName, required this.imageUrl})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 260,
+      height: 150,
       decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.circular(10),
       ),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           GestureDetector(
             onTap: () {
               // Go to the shop page
             },
             child: Container(
-              height: 150,
-              padding: EdgeInsets.all(5),
+              height: 160,
               width: double.infinity,
               child: AspectRatio(
-                  aspectRatio: 1.0,
-                  // child: Icon(Icons.food_bank, color: Colors.black),
-                  child: Image.network(
-                    shopImage,
-                    fit: BoxFit.contain,
-                  )),
+                aspectRatio: 1.0,
+                child: imageUrl != null
+                    ? Image.network(
+                        imageUrl,
+                        fit: BoxFit.cover,
+                      )
+                    : Placeholder(),
+              ),
             ),
           ),
-          Expanded(
-            flex: 2,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    shopName,
-                    style: TextStyle(color: Colors.black),
-                  ),
-                ],
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+            child: Text(
+              shopName,
+              style: const TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
               ),
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
             ),
           ),
         ],
       ),
     );
-    // return Row(children: [
-    //   Expanded(
-    //       child: Container(
-    //           margin: EdgeInsets.symmetric(horizontal: 10),
-    //           height: 260,
-    //           width: 170,
-    //           decoration: BoxDecoration(
-    //             color: Colors.white,
-    //             borderRadius: BorderRadius.circular(10),
-    //           ),
-    //           child: Column(
-    //             children: [
-    //               GestureDetector(
-    //                 onTap: () {
-    //                   //go to the shop page
-    //                 },
-    //                 child: Container(
-    //                   height: 150,
-    //                   padding: EdgeInsets.all(5),
-    //                   width: double.infinity,
-    //                   child: AspectRatio(
-    //                       aspectRatio: 1.0,
-    //                       child: Icon(Icons.food_bank, color: Colors.black)),
-    //                 ),
-    //               ),
-    //               Expanded(
-    //                   flex: 2,
-    //                   child: Padding(
-    //                       padding: const EdgeInsets.symmetric(
-    //                           horizontal: 10, vertical: 10),
-    //                       child: Column(
-    //                         crossAxisAlignment: CrossAxisAlignment.start,
-    //                         children: [
-    //                           Text(shopName,
-    //                               style: TextStyle(color: Colors.black)),
-    //                           SizedBox(height: 5),
-    //                         ],
-    //                       )))
-    //             ],
-    //           )))
-    // ]);
   }
 }
