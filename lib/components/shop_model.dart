8b5e7db3745e0c8823d4a9be12d4edebf9ee5b77
CHIA JEE LIANG <93245148+jeeliang@users.noreply.github.com>
2023-06-01 08:44:31 +0800

    Add files via upload

--- lib/components/shop_model.dart
+++ lib/components/shop_model.dart
@@ -1,97 +1,112 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
+import '../pages/customer/shop_menu.dart';
 
 class ShopModel extends StatelessWidget {
   final String shopName;
-  final String shopImage;
+  final String email;
+  final String imageUrl;
 
-  const ShopModel({super.key, required this.shopName, required this.shopImage});
+  const ShopModel(
+      {Key? key,
+      required this.shopName,
+      required this.email,
+      required this.imageUrl})
+      : super(key: key);
 
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Container(
+  //     height: 150,
+  //     decoration: BoxDecoration(
+  //       color: Colors.white,
+  //       borderRadius: BorderRadius.circular(10),
+  //     ),
+  //     child: Column(
+  //       crossAxisAlignment: CrossAxisAlignment.start,
+  //       children: [
+  //         GestureDetector(
+  //           onTap: () {
+  //             Navigator.push(
+  //                 context,
+  //                 MaterialPageRoute(
+  //                   builder: (context) => MenuPage(email: email),
+  //                 ));
+  //             // Go to the shop page
+  //           },
+  //           child: Container(
+  //             height: 160,
+  //             width: double.infinity,
+  //             child: AspectRatio(
+  //               aspectRatio: 1.0,
+  //               child: imageUrl != null
+  //                   ? Image.network(
+  //                       imageUrl,
+  //                       fit: BoxFit.cover,
+  //                     )
+  //                   : Placeholder(),
+  //             ),
+  //           ),
+  //         ),
+  //         Padding(
+  //           padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+  //           child: Text(
+  //             shopName,
+  //             style: const TextStyle(
+  //               color: Colors.black,
+  //               fontWeight: FontWeight.bold,
+  //               fontSize: 16,
+  //             ),
+  //             maxLines: 2,
+  //             overflow: TextOverflow.ellipsis,
+  //           ),
+  //         ),
+  //       ],
+  //     ),
+  //   );
+  // }
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 260,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Column(
-        children: [
-          GestureDetector(
+    return GestureDetector(
             onTap: () {
-              // Go to the shop page
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => MenuPage(email: email),
+                  ));
             },
-            child: Container(
-              height: 150,
-              padding: EdgeInsets.all(5),
-              width: double.infinity,
-              child: AspectRatio(
-                  aspectRatio: 1.0,
-                  // child: Icon(Icons.food_bank, color: Colors.black),
-                  child: Image.network(
-                    shopImage,
-                    fit: BoxFit.contain,
-                  )),
+      child: Container(
+        
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.white,
+          boxShadow: const [
+            BoxShadow(
+              color: Colors.grey,
+              blurRadius: 2,
+              offset: Offset(0, 2),
             ),
-          ),
-          Expanded(
-            flex: 2,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    shopName,
-                    style: TextStyle(color: Colors.black),
-                  ),
-                ],
+          ],
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.network(
+              imageUrl,
+              height: 100,
+              width: 100,
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(height: 10),
+            Text(
+              shopName,
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
-    // return Row(children: [
-    //   Expanded(
-    //       child: Container(
-    //           margin: EdgeInsets.symmetric(horizontal: 10),
-    //           height: 260,
-    //           width: 170,
-    //           decoration: BoxDecoration(
-    //             color: Colors.white,
-    //             borderRadius: BorderRadius.circular(10),
-    //           ),
-    //           child: Column(
-    //             children: [
-    //               GestureDetector(
-    //                 onTap: () {
-    //                   //go to the shop page
-    //                 },
-    //                 child: Container(
-    //                   height: 150,
-    //                   padding: EdgeInsets.all(5),
-    //                   width: double.infinity,
-    //                   child: AspectRatio(
-    //                       aspectRatio: 1.0,
-    //                       child: Icon(Icons.food_bank, color: Colors.black)),
-    //                 ),
-    //               ),
-    //               Expanded(
-    //                   flex: 2,
-    //                   child: Padding(
-    //                       padding: const EdgeInsets.symmetric(
-    //                           horizontal: 10, vertical: 10),
-    //                       child: Column(
-    //                         crossAxisAlignment: CrossAxisAlignment.start,
-    //                         children: [
-    //                           Text(shopName,
-    //                               style: TextStyle(color: Colors.black)),
-    //                           SizedBox(height: 5),
-    //                         ],
-    //                       )))
-    //             ],
-    //           )))
-    // ]);
   }
 }
