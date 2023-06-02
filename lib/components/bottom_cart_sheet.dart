f12f52a62f3369bad8e92dbb8b317caac747d960
Amira Azkia <99363263+Mir707@users.noreply.github.com>
2023-06-03 06:37:52 +0800

    Update bottom_cart_sheet.dart

--- lib/components/bottom_cart_sheet.dart
+++ lib/components/bottom_cart_sheet.dart
@@ -3,8 +3,25 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'controller.dart';
 
-class BottomCartSheet extends StatelessWidget {
-  final Controller c = Get.put(Controller());
+class BottomCartSheet extends StatefulWidget {
+  final String productName;
+  final String productImage;
+  final double productPrice;
+
+  const BottomCartSheet({
+    Key? key,
+    required this.productName,
+    required this.productImage,
+    required this.productPrice,
+  }) : super(key: key);
+
+  @override
+  State<BottomCartSheet> createState() => _BottomCartSheetState();
+}
+
+class _BottomCartSheetState extends State<BottomCartSheet> {
+  final Controller c = Get.find();
+
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -19,138 +36,135 @@ class BottomCartSheet extends StatelessWidget {
               child: SingleChildScrollView(
                 child: Column(
                   children: [
-                    for (int i = 1; i < 8; i++)
-                      Container(
-                        margin:
-                            EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            color: Colors.white,
-                            boxShadow: [
-                              BoxShadow(
-                                  color: Colors.grey.withOpacity(0.5),
-                                  spreadRadius: 1,
-                                  blurRadius: 8),
-                            ]),
-                        child: Row(
-                          children: [
-                            Padding(
-                              padding: EdgeInsets.all(10),
-                              child: Image.asset(
-                                "lib/images/merantiLogo.png",
-                                height: 80,
-                                width: 80,
-                              ),
+                    Container(
+                      margin:
+                          EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 1,
+                            blurRadius: 8,
+                          ),
+                        ],
+                      ),
+                      child: Row(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.all(10),
+                            child: Image.network(
+                              widget.productImage,
+                              height: 80,
+                              width: 80,
                             ),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Container(
-                                  margin: EdgeInsets.only(bottom: 15),
-                                  child: Text(
-                                    "PRODUCT NAME",
-                                    style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.bold,
-                                    ),
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                margin: EdgeInsets.only(bottom: 15),
+                                child: Text(
+                                  widget.productName,
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
                                   ),
                                 ),
-                                Container(
-                                  child: Text(
-                                    "PRODUCT PRICE",
-                                    style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.bold,
-                                    ),
+                              ),
+                              Container(
+                                child: Text(
+                                  "RM ${widget.productPrice.toStringAsFixed(2)}",
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
                                   ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          Spacer(),
+                          Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 10),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                Icon(
+                                  Icons.delete,
+                                  color: Colors.black,
+                                  size: 24,
+                                ),
+                                SizedBox(height: 25),
+                                Row(
+                                  children: [
+                                    Container(
+                                      height: 40,
+                                      width: 40,
+                                      decoration: BoxDecoration(
+                                        color: Colors.white,
+                                        borderRadius: BorderRadius.circular(25),
+                                        boxShadow: [
+                                          BoxShadow(
+                                            color: Colors.grey.withOpacity(0.5),
+                                            spreadRadius: 2,
+                                            blurRadius: 8,
+                                          ),
+                                        ],
+                                      ),
+                                      child: IconButton(
+                                        icon: Icon(
+                                          Icons.remove,
+                                          size: 22,
+                                        ),
+                                        onPressed: () => c.decrement(),
+                                      ),
+                                    ),
+                                    Container(
+                                      margin:
+                                          EdgeInsets.symmetric(horizontal: 10),
+                                      child: Obx(
+                                        () => Text(
+                                          "${c.products.toString()}",
+                                          style: TextStyle(
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                    Container(
+                                      height: 40,
+                                      width: 40,
+                                      decoration: BoxDecoration(
+                                        color: Colors.white,
+                                        borderRadius: BorderRadius.circular(25),
+                                        boxShadow: [
+                                          BoxShadow(
+                                            color: Colors.grey.withOpacity(0.5),
+                                            spreadRadius: 2,
+                                            blurRadius: 8,
+                                          ),
+                                        ],
+                                      ),
+                                      child: IconButton(
+                                        icon: Icon(
+                                          Icons.add,
+                                          size: 22,
+                                        ),
+                                        onPressed: () => c.increment(),
+                                      ),
+                                    ),
+                                  ],
                                 ),
                               ],
                             ),
-                            Spacer(),
-                            Padding(
-                              padding: EdgeInsets.symmetric(horizontal: 10),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.end,
-                                children: [
-                                  Icon(
-                                    Icons.delete,
-                                    color: Colors.black,
-                                    size: 24,
-                                  ),
-                                  SizedBox(height: 25),
-                                  Row(
-                                    children: [
-                                      Container(
-                                        height: 40,
-                                        width: 40,
-                                        decoration: BoxDecoration(
-                                          color: Colors.white,
-                                          borderRadius:
-                                              BorderRadius.circular(25),
-                                          boxShadow: [
-                                            BoxShadow(
-                                              color:
-                                                  Colors.grey.withOpacity(0.5),
-                                              spreadRadius: 2,
-                                              blurRadius: 8,
-                                            ),
-                                          ],
-                                        ),
-                                        child: IconButton(
-                                          icon: Icon(
-                                            Icons.remove,
-                                            size: 22,
-                                          ),
-                                          onPressed: () => c.decrement(),
-                                        ),
-                                      ),
-                                      Container(
-                                        margin: EdgeInsets.symmetric(
-                                            horizontal: 10),
-                                        child: Obx(
-                                          () => Text(
-                                            "${c.products.toString()}",
-                                            style: TextStyle(
-                                              fontSize: 18,
-                                              fontWeight: FontWeight.bold,
-                                            ),
-                                          ),
-                                        ),
-                                      ),
-                                      Container(
-                                        height: 40,
-                                        width: 40,
-                                        decoration: BoxDecoration(
-                                          color: Colors.white,
-                                          borderRadius:
-                                              BorderRadius.circular(25),
-                                          boxShadow: [
-                                            BoxShadow(
-                                              color:
-                                                  Colors.grey.withOpacity(0.5),
-                                              spreadRadius: 2,
-                                              blurRadius: 8,
-                                            ),
-                                          ],
-                                        ),
-                                        child: IconButton(
-                                          icon: Icon(
-                                            Icons.add,
-                                            size: 22,
-                                          ),
-                                          onPressed: () => c.increment(),
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
+                    ),
                   ],
                 ),
               ),
@@ -173,7 +187,7 @@ class BottomCartSheet extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Text(
-                    "RM\$ ",
+                    "RM ${widget.productPrice.toStringAsFixed(2)}",
                     style: TextStyle(
                       fontSize: 24,
                       color: Colors.black,
