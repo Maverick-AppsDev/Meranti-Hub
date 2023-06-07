e3d7ba491422482ce6d284f50849f29c9ac2f20c
CHIA JEE LIANG <93245148+jeeliang@users.noreply.github.com>
2023-06-07 21:25:06 +0800

    Add files via upload

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -1,19 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:sprint1/components/rounded_button.dart';
 import 'package:sprint1/components/background.dart';
-import 'package:sprint1/pages/customer/qr_page.dart';
 import 'package:sprint1/pages/seller/auth_page.dart';
 import 'package:sprint1/components/constant.dart';
-// import 'package:sprint1/pages/customer/cust_page2.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:sprint1/pages/customer/cust_page2.dart';
 
 class WelcomePage extends StatelessWidget {
   const WelcomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       backgroundColor: kBackgroundColor,
-      body: const WelcomeScreen(),
+      body: WelcomeScreen(),
     );
   }
 }
@@ -23,9 +24,52 @@ class WelcomeScreen extends StatefulWidget {
 
   @override
   State<WelcomeScreen> createState() => _WelcomeScreenState();
+
 }
 
 class _WelcomeScreenState extends State<WelcomeScreen> {
+  String _scanRes = 'unknown';
+
+  Future<void> scanQR() async {
+    String qrScanRes;
+    try {
+      qrScanRes = await FlutterBarcodeScanner.scanBarcode(
+        '#ff6666',
+        'Cancel',
+        true,
+        ScanMode.QR,
+      );
+      debugPrint(qrScanRes);
+    } on PlatformException {
+      qrScanRes = 'Failed to get platform version';
+    }
+
+    if (!mounted) return;
+
+    setState(() {
+      _scanRes = qrScanRes;
+    });
+
+    int resNum = 0;
+    try {
+      resNum = int.parse(_scanRes);
+    } catch (e) {
+      print('Invalide QR code');
+      qrScanRes = 'Invalid QR code';
+    }
+
+    // To be use to send the QR to the next page
+    if (resNum > 0 && resNum < 10) {
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (BuildContext context) {
+            return CustomerPage(tableNum: resNum);
+          },
+        ),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -47,14 +91,14 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
             RoundedButton(
               text: "Customer",
               press: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (BuildContext context) {
-                      return const QRPage(status: 'entered qr page page',);
-                    },
-                  ),
-                );
-                // debugPrint("Customer Button");
+                scanQR();
+                // Navigator.of(context).push(
+                //   MaterialPageRoute(
+                //     builder: (BuildContext context) {
+                //       return const QRPage(status: 'entered qr page page',);
+                //     },
+                //   ),
+                // );
               },
             ),
             RoundedButton(
