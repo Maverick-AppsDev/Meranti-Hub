fb6f436f4639238d44471c78043bc7f99e626e42
Amira Azkia <99363263+Mir707@users.noreply.github.com>
2023-06-19 12:39:23 +0800

    Update order_page.dart

--- lib/pages/seller/order_page.dart
+++ lib/pages/seller/order_page.dart
@@ -29,56 +29,74 @@ class _OrderPageState extends State<OrderPage> {
   // Widget to show the order using a listed boxes
   Widget buildOrder() {
     return StreamBuilder<List<FoodOrder>>(
-        stream: readOrder(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            final orders = snapshot.data!;
-            // Orders by table num
-            final Map<int, List<FoodOrder>> groupedOrders = {};
-            for (final order in orders) {
-              if (groupedOrders.containsKey(order.tableNum)) {
-                groupedOrders[order.tableNum]!.add(order);
-              } else {
-                groupedOrders[order.tableNum] = [order];
-              }
+      stream: readOrder(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          final orders = snapshot.data!;
+          // Orders by table num
+          final Map<int, List<FoodOrder>> groupedOrders = {};
+          for (final order in orders) {
+            if (groupedOrders.containsKey(order.tableNum)) {
+              groupedOrders[order.tableNum]!.add(order);
+            } else {
+              groupedOrders[order.tableNum] = [order];
             }
-
-            return ListView.builder(
-                itemCount: groupedOrders.length,
-                itemBuilder: (context, index) {
-                  final tableNum = groupedOrders.keys.elementAt(index);
-                  final ordersForTable = groupedOrders[tableNum]!;
-
-                  return Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Padding(
-                            padding: const EdgeInsets.all(8),
-                            child: Text(
-                              'Table $tableNum',
-                              style: TextStyle(
-                                  fontSize: 18, fontWeight: FontWeight.bold),
-                            )),
-                        ListView.builder(
-                            shrinkWrap: true,
-                            physics: NeverScrollableScrollPhysics(),
-                            itemCount: ordersForTable.length,
-                            itemBuilder: (context, index) {
-                              final order = ordersForTable[index];
-                              return ListTile(
-                                  leading: Image.network(order.foodImgUrl),
-                                  title: Text(order.name),
-                                  subtitle: Text(
-                                      'RM ${order.price.toStringAsFixed(2)}'));
-                            })
-                      ]);
-                });
-          } else if (snapshot.hasError) {
-            return Text('Error: ${snapshot.error}');
-          } else {
-            return CircularProgressIndicator();
           }
-        });
+
+          return ListView.builder(
+            itemCount: groupedOrders.length,
+            itemBuilder: (context, index) {
+              final tableNum = groupedOrders.keys.elementAt(index);
+              final ordersForTable = groupedOrders[tableNum]!;
+
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: Text(
+                      'Table $tableNum',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  ListView.builder(
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    itemCount: ordersForTable.length,
+                    itemBuilder: (context, index) {
+                      final order = ordersForTable[index];
+                      return Card(
+                        child: ListTile(
+                          leading: Image.network(order.foodImgUrl),
+                          title: Text(order.name),
+                          subtitle:
+                              Text('RM ${order.price.toStringAsFixed(2)}'),
+                          trailing: IconButton(
+                            icon: Icon(Icons.check_circle),
+                            iconSize: 40,
+                            onPressed: () {
+                              // Logic to delete the order
+                              // You can call a function here to delete the order
+                            },
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              );
+            },
+          );
+        } else if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        } else {
+          return CircularProgressIndicator();
+        }
+      },
+    );
   }
 
   @override
