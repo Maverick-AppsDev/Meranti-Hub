487f8667a016bf98b1cdfc5ef4fca1fc009dc140
Amira Azkia <99363263+Mir707@users.noreply.github.com>
2023-06-19 20:43:43 +0800

    Update payment_tng.dart

--- lib/pages/customer/payment_tng.dart
+++ lib/pages/customer/payment_tng.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:sprint1/components/constant.dart';
 import 'package:sprint1/pages/customer/payment_page.dart';
 
+import 'order_page_cust.dart';
+
 class PaymentTNG extends StatefulWidget {
   final String email;
-  final int tableNum;
 
-  const PaymentTNG({Key? key, required this.email, required this.tableNum})
-      : super(key: key);
+  const PaymentTNG({super.key, required this.email});
 
   @override
   State<PaymentTNG> createState() => _PaymentTNGState();
@@ -17,54 +17,68 @@ class _PaymentTNGState extends State<PaymentTNG> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: kBackgroundColor,
-      appBar: AppBar(
-        backgroundColor: const Color(0xfffd2e6),
-        title: const Text('Touch n Go payment'),
-        actions: [],
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(height: 25),
-            Container(
-              child: Image(
-                image: AssetImage("lib/images/dummy_tng.jpg"),
+        backgroundColor: kBackgroundColor,
+        appBar: AppBar(
+          backgroundColor: const Color(0xfffd2e6),
+          title: const Text('Touch n Go payment'),
+          actions: [],
+        ),
+        body: SingleChildScrollView(
+            child: Column(children: [
+          SizedBox(height: 25),
+          Container(
+            child: Image(
+              image: AssetImage("lib/images/dummy_tng.jpg"),
+            ),
+          ),
+          SizedBox(height: 25),
+          Container(
+            child: Text("Use the link below to pay"),
+            alignment: Alignment.center,
+          ),
+          Container(
+            child: Text(
+              "https:link.tngdigital.com.my/thisIsDummyLink",
+              style: TextStyle(
+                color: Colors.blue,
+                decoration: TextDecoration.underline,
               ),
             ),
-            SizedBox(height: 25),
-            Container(
-              child: Text("Use the link below to pay"),
-              alignment: Alignment.center,
+            alignment: Alignment.center,
+          ),
+          SizedBox(height: 10),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.pink[300], // Set the desired color
+              borderRadius:
+                  BorderRadius.circular(10), // Set the desired border radius
             ),
-            Container(
-              child: Text(
-                "https:link.tngdigital.com.my/thisIsDummyLink",
-                style: TextStyle(
-                  color: Colors.blue,
-                  decoration: TextDecoration.underline,
-                ),
-              ),
-              alignment: Alignment.center,
-            ),
-            SizedBox(height: 10),
-            ElevatedButton(
-              // redirect to tracking page
+            child: ElevatedButton(
               onPressed: () {
                 Navigator.of(context).pop();
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: (context) => PaymentPage(
-                        email: widget.email, tableNum: widget.tableNum),
+                    builder: (context) => OrderPage(
+                      email: widget.email,
+                    ),
                   ),
                 );
               },
-              child: Text("Finish Payment"),
-            )
-          ],
-        ),
-      ),
-    );
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.pink[300],
+                elevation: 0, // Remove the button's elevation
+              ),
+              child: Text(
+                "Finish Payment",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          )
+        ])));
   }
 }
