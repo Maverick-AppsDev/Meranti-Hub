855e5f9eb38305285fe97c9800c351d1c63cf802
Amira Azkia <99363263+Mir707@users.noreply.github.com>
2023-06-19 11:49:34 +0800

    New version of bottom_cart_sheet.dart

--- lib/components/bottom_cart_sheet.dart
+++ lib/components/bottom_cart_sheet.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import '../pages/customer/payment_page.dart';
 import 'controller.dart';
+import 'package:square_in_app_payments/models.dart';
+import 'package:square_in_app_payments/in_app_payments.dart';
 
 class BottomCartSheet extends StatefulWidget {
-  const BottomCartSheet({super.key});
+  final String email;
+  final int tableNum;
+  const BottomCartSheet(
+      {super.key, required this.email, required this.tableNum});
 
   @override
   State<BottomCartSheet> createState() => _BottomCartSheetState();
@@ -15,15 +21,17 @@ class _BottomCartSheetState extends State<BottomCartSheet> {
 
   @override
   Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+
     return Material(
       child: Container(
         color: Color.fromARGB(255, 248, 163, 191),
         padding: EdgeInsets.only(top: 20),
-        height: 600,
+        height: screenHeight * 0.8,
         child: Column(
           children: [
             Container(
-              height: 382,
+              height: screenHeight * 0.4,
               child: SingleChildScrollView(
                 child: Column(
                   children: [
@@ -31,6 +39,7 @@ class _BottomCartSheetState extends State<BottomCartSheet> {
                       Container(
                         margin:
                             EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                        height: 120,
                         decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(10),
                           color: Colors.white,
@@ -45,18 +54,18 @@ class _BottomCartSheetState extends State<BottomCartSheet> {
                         child: Row(
                           children: [
                             Padding(
-                              padding: EdgeInsets.all(10),
+                              padding: EdgeInsets.all(6),
                               child: Image.network(
                                 c.cartItems[i].productImage,
-                                height: 80,
-                                width: 80,
+                                height: 90,
+                                width: 58,
                               ),
                             ),
                             Column(
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
                                 Container(
-                                  margin: EdgeInsets.only(bottom: 15),
+                                  margin: EdgeInsets.only(top: 20),
                                   child: Text(
                                     c.cartItems[i].productName,
                                     style: TextStyle(
@@ -67,6 +76,7 @@ class _BottomCartSheetState extends State<BottomCartSheet> {
                                   ),
                                 ),
                                 Container(
+                                  padding: EdgeInsets.only(top: 10),
                                   child: Text(
                                     "RM ${c.cartItems[i].productPrice.toStringAsFixed(2)}",
                                     style: TextStyle(
@@ -96,7 +106,7 @@ class _BottomCartSheetState extends State<BottomCartSheet> {
                                       });
                                     },
                                   ),
-                                  SizedBox(height: 25),
+                                  SizedBox(height: 20),
                                   Row(
                                     children: [
                                       Container(
@@ -195,37 +205,72 @@ class _BottomCartSheetState extends State<BottomCartSheet> {
                 ],
               ),
               child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    "RM ${c.totalPrice.toStringAsFixed(2)}",
-                    style: TextStyle(
-                      fontSize: 24,
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  InkWell(
-                    onTap: () {},
-                    child: Container(
-                      padding:
-                          EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                      decoration: BoxDecoration(
-                        color: Color.fromARGB(255, 240, 98, 146),
-                        borderRadius: BorderRadius.circular(30),
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "RM ${c.totalPrice.toStringAsFixed(2)}",
+                      style: TextStyle(
+                        fontSize: 24,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
                       ),
-                      child: Text(
-                        "Check Out",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
+                    ),
+                    InkWell(
+                      onTap: () {
+                        c.saveOrder(widget.email, widget.tableNum);
+                      },
+                      child: ElevatedButton(
+                        onPressed: () {
+                          // show the pop up window
+                          showDialog(
+                              context: context,
+                              builder: (context) {
+                                return AlertDialog(
+                                    title:
+                                        Text("Are you sure about your order?"),
+                                    actions: [
+                                      TextButton(
+                                        onPressed: () {
+                                          // Perform the action when 'Yes' is pressed
+                                          Navigator.of(context).pop();
+                                          Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                              builder: (context) => PaymentPage(
+                                                  email: widget.email,
+                                                  tableNum: widget.tableNum),
+                                            ),
+                                          );
+                                        },
+                                        child: Text("Yes"),
+                                      ),
+                                      TextButton(
+                                        onPressed: () {
+                                          // Perform the action when 'No' is pressed
+                                          Navigator.of(context).pop();
+                                        },
+                                        child: Text("No"),
+                                      ),
+                                    ]);
+                              });
+                        },
+                        style: ElevatedButton.styleFrom(
+                            padding: EdgeInsets.symmetric(
+                                vertical: 10, horizontal: 15),
+                            backgroundColor: Color.fromARGB(255, 240, 98, 146),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(30))),
+                        child: Text(
+                          "Check Out",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ],
-              ),
+                  ]),
             ),
           ],
         ),
